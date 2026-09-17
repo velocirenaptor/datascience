72d700bd15a078bf667023d6c5caa3dafae11b27
Renata Mota <159165553+velocirenaptor@users.noreply.github.com>
2026-09-17 14:16:49 +0000

    dashboard

--- hello-dash.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import pandas as pd 
-st.title("hello phucker")
-st.write("hello world streamlit")
-dataframe = pd.read_csv("https://raw.githubusercontent.com/adsoftsito/ciencia-datos/refs/heads/main/titanic.csv")
-st.dataframe(dataframe)
